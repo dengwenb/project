23fb22384cb40e23933551199c9cb24fb78499f8
ls <1253658502@qq.com>
2018-11-15 19:39:07 +0800

    ls

--- routes/web.php
+++ routes/web.php
@@ -21,24 +21,7 @@ Route::get('/pic', function () {
 Route::get('/login',function(){
 	return view('login');
 });
-// 后台模块
-Route::resource('/admin','Admin\AdminController');
-//后台管理员模块
-Route::resource('/adminstrator','Admin\AdministratorsController');
-// 后台会员模块
-Route::resource('/adminuser','Admin\UserController');
-//后台订单管理
-Route::resource('/adminorder','Admin\OrderController');
-//后台设置模块
-Route::resource('/adminsystem','Admin\SystemController');
-//后台用户评论模块
-Route::resource('/adminreviews','Admin\ReviewsController');
-//后台图片管理
-Route::resource('/adminpicture','Admin\PictureController');
-//后台商品管理模块
-Route::resource('/adminshop','Admin\ShopController');
-//后台查看商品详情
-Route::resource('/adminshopinfo','Admin\ShopinfoController');
+
 
 
 Route::group(["middleware"=>"index"],function(){
